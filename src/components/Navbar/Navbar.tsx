1365c98c5a177c0de8837d5b717721c27fdf6f85
Blake R <blakeromano19@gmail.com>
2021-09-04 19:13:27 -0400

    stub

---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,16 @@
+import React, { FunctionComponent, useState } from 'react'
+import { Link } from 'react-router-dom'
+
+interface NavbarProps {
+  
+}
+ 
+const Navbar: FunctionComponent<NavbarProps> = (props) => {
+  return (
+    <>
+    <h1>Navbar!</h1>
+    </>
+  );
+}
+ 
+export default Navbar;
---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,15 @@
+import React, { useState, useEffect, FunctionComponent } from 'react'
+
+interface HomeProps {
+  
+}
+ 
+const Home: FunctionComponent<HomeProps> = (props) => {
+  return (
+    <>
+    <h1>Home!</h1>
+    </>
+  );
+}
+ 
+export default Home;
